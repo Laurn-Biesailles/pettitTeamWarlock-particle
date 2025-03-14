7f5d47872da31e3a7445f8e08acda9da1b26d053
Laurn-Biesailles <pettitjustust@gmail.com>
2025-03-14 16:55:18 -0700

    fixed node

--- node.h
+++ node.h
@@ -1,30 +1,33 @@
-#include <>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include "particle.h"
 using namespace std;
 
 class node {
-	particle p;
+	Particle p;
 	node *next = nullptr;
 	node *prev = nullptr;
 	public:
-	node (particle newP, node *newNext = nullptr, node *newPrev = nullptr) {
+	node (Particle newP, node *newNext = nullptr, node *newPrev = nullptr) {
 		p = newP;
 		next = newNext;
 		prev = newPrev;
 	}
-	particle getP () const {
+	Particle getP () const {
 		return p;
 	}
-	void setP (particle newP) {
+	void setP (Particle newP) {
 		p = newP;
 	}
 	node getNext () const {
-		return next;
+		return *next;
 	}
 	void setNext (node *newNext = nullptr) {
 		next = newNext;
 	}
 	node getPrev () const {
-		return prev;
+		return *prev;
 	}
 	void setPrev (node *newPrev = nullptr) {
 		prev = newPrev;
