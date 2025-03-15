ba7f0cec9c339e5c3cd37939fb4b37707adafe75
Laurn-Biesailles <pettitjustust@gmail.com>
2025-03-14 17:04:49 -0700

    minor formatting changes to node.h

--- node.h
+++ node.h
@@ -5,31 +5,25 @@
 using namespace std;
 
 class node {
+	// particle variable
 	Particle p;
-	node *next = nullptr;
-	node *prev = nullptr;
+	// linking variables
+	node *next,*prev = nullptr;
+
 	public:
+	// constructor
 	node (Particle newP, node *newNext = nullptr, node *newPrev = nullptr) {
 		p = newP;
 		next = newNext;
 		prev = newPrev;
 	}
-	Particle getP () const {
-		return p;
-	}
-	void setP (Particle newP) {
-		p = newP;
-	}
-	node getNext () const {
-		return *next;
-	}
-	void setNext (node *newNext = nullptr) {
-		next = newNext;
-	}
-	node getPrev () const {
-		return *prev;
-	}
-	void setPrev (node *newPrev = nullptr) {
-		prev = newPrev;
-	}
+	// particle getter/setter
+	Particle getP () const {return p;}
+	void setP (Particle newP) {p = newP;}
+	// next link getter/setter
+	node getNext () const {return *next;}
+	void setNext (node *newNext = nullptr) {next = newNext;}
+	// previous link getter/setter
+	node getPrev () const {return *prev;}
+	void setPrev (node *newPrev = nullptr) {prev = newPrev;}
 };
