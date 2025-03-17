95c0c60ca21ec8155bd53eefab99a1bc7ee29b0d
pettit_0982143 <0982143@my.scccd.edu>
2025-03-17 13:40:09 -0700

    fixing node class

--- main.cc
+++ main.cc
@@ -6,11 +6,15 @@
 
 int main () {
     cout << "Particle System Testing Phase" << endl;
+
     cout << "Checking particle class" << endl;
+
     cout << "Checking linked list node class" << endl;
     nodeTest();
     cout << "All tests passed." << endl;
+
     cout << "Checking particle system class" << endl;
+
     cout << "Checking particle graphics class" << endl;
 	ParticleGraphics_test();
 }
