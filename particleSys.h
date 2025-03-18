825e5aae3544f72e1d86fb5b781588a6099a8cd3
aubuchon_0747497 <therealdanblan@gmail.com>
2025-03-17 19:36:14 -0700

    Updated particleSys.h

--- particleSys.h
+++ particleSys.h
@@ -1,44 +1,104 @@
 #pragma once
 #include <iostream>
 #include "node.h"
+#include "particle.h"
 #include "ParticleGraphics.h"
 
 
 class particleSystem{
 	auto row = 480;
 	auto colm = 360;
-	List particles;	
+	node *head = nullptr;
+	node *tail = nullptr;
+	int size = 0;
 	public:
+	
+	particleSystem(){}
+
+	~particleSystem(){
+		 node *temp = head;
+         while(temp){
+            node *temp2 = temp->getNext();
+            delete temp;
+            temp = temp2;
+         }
+	}
+
+
 	void add_particle(Particle particle){
-		particles.push_back(particle);
+		node *temp = new node(particle);
+        if (!size) {
+            tail = head = temp;
+        } else {
+            temp->setPrev(tail);
+            tail->setNext(temp);
+            tail = temp;
+        }
+        size++;
+
 	}
 	int numParticles(){
-		return particles.getSize();
+		return size;
 	}
 	void moveParticles(){
-		return;
+		node *temp = head;
+         while(temp){
+			temp.getPart().physics();
+            temp = temp->getNext();
+         }
+
 	}
 	void drawParticles(ParticleGraphics g){
-		return;
+		 node *temp = head;
+         while(temp){
+			g.drawPoint(temp.getPart().getX(),temp.getPart().getY());
+            temp = temp->getNext();
+         }
+
 	}
-
-
-	void partSysTest(){
-		ParticleSystem ps;
-		int size = ps.numParticles();
-		if(size != 0){
-			cout << "Incorrectly created particle system" << endl;
-		}
-		particle a;
-		ps.add_particle(a);
-		size = ps.numParticles();
-		if(size !> 0){
-			cout << "Did not add particle" << endl;
-		}
-		else{
-			cout << "Added Particle" << endl;
-		}
-	}
-	
-
 };
+
+
+
+//TESTING
+
+
+    void partSysTest(){
+        ParticleSystem ps;
+        int size = ps.numParticles();
+        if(size != 0){
+            cout << "Incorrectly created particle system" << endl;
+        }
+        Particle a;
+        ps.add_particle(a);
+        size = ps.numParticles();
+        if(size !> 0){
+            cout << "Did not add particle" << endl;
+        }
+        else{
+            cout << "Added Particle" << endl;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
