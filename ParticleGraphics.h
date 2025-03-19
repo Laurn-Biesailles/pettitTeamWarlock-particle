b1707c98bbbac6235b0a3e3403799617c648e305
siadto_0923258 <303faerie@gmail.com>
2025-03-19 14:59:48 -0700

    Updated draw functions in ParticleGraphics.h

--- ParticleGraphics.h
+++ ParticleGraphics.h
@@ -43,17 +43,72 @@ class ParticleGraphics {
         void drawPoint(int x, int y) {
 			resetcolor();
 			setbgcolor(r,g,b);
-			int row = y;
 			int col = x;
+			int row = y;
 			cout << " ";
 			cout.flush();
 			resetcolor();
 		} // draws particle point
 
-        void drawRectangle(double x, double y) { return;} // draws rectangle
-        void drawOval(double x, double y) { return; } // draws oval
-        void drawPolygon(double x, double y) { return; } // draws polygon
-        void drawLine(double x, double y) { return; } // draws line
+        void drawRectangle(int min_x, int max_x, int min_y, int max_y) { 
+			assert(min_x < max_x);
+			assert(min_y < max_y);
+			resetcolor();
+			setbgcolor(r,g,b);
+			for (int col = min_x; col <= max_x; col++) {
+				for (int row = min_y; row <= max_y; row++) {
+					movecursor(row,col);
+					cout << " ";
+				}
+			}
+			cout.flush();
+			reesetcolor();
+		} // draws rectangle
+
+        void drawOval(int min_x, int max_x, int min_y, int max_y) { 
+			assert(min_x < max_x);
+			assert(min_y < max_y);
+			resetcolor();
+			setbgcolor(r,g,b);
+			for (int col = min_x; col <= max_x; col++) {
+				for (int row = min_y; row <= max_y; row++) {
+					movecursor(row,col);
+					cout << " ";
+				}
+			}
+			cout.flush();
+			resetcolor();
+		} // draws oval - not done!
+
+        void drawPolygon(int min_x, int max_x, int min_y, int max_y) { 
+			assert(min_x < max_x);
+			assert(min_y < max_y);
+			resetcolor();
+			setbgcolor(r,g,b);
+			for (int col = min_x; col <= max_x; col++) {
+				for (int row = min_y; row <= max_y; row++) {
+					movecursor(row,col);
+					cout << " ";
+				}
+			}
+			cout.flush();
+			resetcolor();
+			return; 
+		} // draws polygon - not done!
+
+        void drawLine(int x, int y) { 
+			resetcolor();
+			setbgcolor(r,g,b);
+			int col = x;
+			int row = y;
+			if (col == 1);
+			if (row == 1);
+			cout << " ";
+			cout.flush();
+			resetcolor();
+			return; 
+		} // draws line - not done!
+
 };
 
 
@@ -62,5 +117,8 @@ void ParticleGraphics_test() {
 	if (a.get_r() != 0 || a.get_g() != 0 || a.get_b() != 0) {
 		cout << "Color in constructor or get function does not work. Please fix." << endl;
 		exit (1);
+	}
+	else {
+		cout << "All Tests Passed" << endl;
 	}
 }
