472c241dd192b353801905c61d6bd6ce37b48cc2
aubuchon_0747497 <therealdanblan@gmail.com>
2025-03-23 14:43:36 -0700

    Made changes to main.cc and particleSys.h to allow for terminal drawing

--- particleSys.h
+++ particleSys.h
@@ -3,38 +3,43 @@
 #include "node.h"
 #include "particle.h"
 #include "ParticleGraphics.h"
-
+#include <sys/ioctl.h>
+#include <utility>
+#include <unistd.h>
 
 class particleSystem{
-	int row = 480;
-	int colm = 360;
+	int row = 0 ;
+	int colm = 0;
 	node *head;
 	node *tail;
 	int size = 0;
 	public:
-	
+
 	particleSystem(){}
 
 	~particleSystem(){
-		 node *temp = head;
-         while(temp){
-            node *temp2 = temp->getNext();
-            delete temp;
-            temp = temp2;
-         }
+		node *temp = head;
+		while(temp){
+			node *temp2 = temp->getNext();
+			delete temp;
+			temp = temp2;
+		}
 	}
+
+	//get size for row and colm
+	// auto [rows,cols] = get_terminal_size();
 
 
 	void add_particle(Particle particle){
 		node *temp = new node(particle);
-        if (!size) {
-            tail = head = temp;
-        } else {
-            temp->setPrev(tail);
-            tail->setNext(temp);
-            tail = temp;
-        }
-        size++;
+		if (!size) {
+			tail = head = temp;
+		} else {
+			temp->setPrev(tail);
+			tail->setNext(temp);
+			tail = temp;
+		}
+		size++;
 
 	}
 	int numParticles(){
@@ -42,20 +47,20 @@ class particleSystem{
 	}
 	void moveParticles(){
 		node *temp = head;
-         while(temp){
-		Particle temp2 = temp->getPart();
-		temp2.physics(temp2);
-		temp->setPart(temp2);
-            temp = temp->getNext();
-         }
+		while(temp){
+			Particle temp2 = temp->getPart();
+			temp2.physics(temp2);
+			temp->setPart(temp2);
+			temp = temp->getNext();
+		}
 
 	}
 	void cull(){
 		node *temp = head;
 		while(temp){
 			if(temp->getPart().getX() > row || temp->getPart().getY() > colm ||
-			   temp->getPart().getX() < 0 || temp->getPart().getY() < 0 ||
-			   temp->getPart().getLife() <= 0 ){
+					temp->getPart().getX() < 0 || temp->getPart().getY() < 0 ||
+					temp->getPart().getLife() <= 0 ){
 				if(size == 1){
 					head = tail = nullptr;
 					delete temp;
@@ -63,11 +68,11 @@ class particleSystem{
 					size--;
 				}
 				else if(temp == head){
-				head = temp->getNext();
-				head->setPrev(nullptr);
-				delete temp;
-				temp = head;
-				size--;
+					head = temp->getNext();
+					head->setPrev(nullptr);
+					delete temp;
+					temp = head;
+					size--;
 				}
 				else if(temp == tail){
 					tail = temp->getPrev();
@@ -77,53 +82,89 @@ class particleSystem{
 					size--;
 				}
 				else{
-					 node *temp2 = temp->getPrev();
-                     node *temp3 = temp->getNext();
-                     temp2->setNext(temp3);
-                     temp3->setPrev(temp2);
-					 delete temp;
-					 temp = temp3;
+					node *temp2 = temp->getPrev();
+					node *temp3 = temp->getNext();
+					temp2->setNext(temp3);
+					temp3->setPrev(temp2);
+					delete temp;
+					temp = temp3;
+					size--;
 				}
 
-				 
+
 			}
 			else{
 				temp = temp->getNext();
 			}
 		}
 	}
-
+//normal draw
 	void drawParticles(ParticleGraphics g){
-		 node *temp = head;
-         while(temp){
+		node *temp = head;
+		while(temp){
 			g.drawPoint(temp->getPart().getX(),temp->getPart().getY());
-            temp = temp->getNext();
-         }
+			cout << "Drawing at (" << temp->getPart().getX() << ", " << temp->getPart().getY() << ")\n";//
+			temp = temp->getNext();
+		}
 
 	}
+	void updateBounds() {
+		auto [r, c] = get_terminal_size();
+		row = r;
+		colm = c;
+	}
+
+
+	void runFrame(ParticleGraphics& g) {
+		while(size >= 1){
+		clearscreen();
+	    updateBounds();        // Make sure rows and cols are fresh
+		moveParticles();       // Step 1: Apply physics
+		cull();                // Step 2: Remove dead/out-of-bounds
+		drawParticles(g);      // Step 3: Draw remaining
+		usleep(100090);
+		}
+	}
+
+//This function is for program C
+	void runFrameC(ParticleGraphics& g) {
+		std::cout << "\033[?25l"; // 👈 Hide cursor        
+        clearscreen();
+        updateBounds();        // Make sure rows and cols are fresh
+        moveParticles();       // Step 1: Apply physics
+        cull();                // Step 2: Remove dead/out-of-bounds
+        drawParticles(g);      // Step 3: Draw remaining
+        usleep(100090);
+        
+    }
+
+
+
+
 };
+
 
 
 
 //TESTING
 
 
-    void partSysTest(){
-        particleSystem ps;
-        int size = ps.numParticles();
-        if(size != 0){
-            cout << "Incorrectly created particle system" << endl;
-        }
-        Particle a;
-        ps.add_particle(a);
-        size = ps.numParticles();
-        if(size <= 0){
-            cout << "Did not add particle" << endl;
-        }
-        else{
-            cout << "Added Particle" << endl;
-        }
-    }
+void partSysTest(){
+	particleSystem ps;
+	int size = ps.numParticles();
+	if(size != 0){
+		cout << "Incorrectly created particle system" << endl;
+	}
+	Particle a;
+	ps.add_particle(a);
+	size = ps.numParticles();
+	if(size <= 0){
+		cout << "Did not add particle" << endl;
+	}
+	else{
+		cout << "Added Particle" << endl;
+	}
+}
 
 
 
